89bf66ebe5f166e3f5ba9c0f204ff12a0eaf40f6
by09115 <by09115@outlook.kr>
2019-06-08 21:38:18 +0900

    EDIT: I'm thinking about the redis connection pool.

--- chanel/config/vault.py
+++ chanel/config/vault.py
@@ -1,9 +1,13 @@
+from typing import Dict
 from hvac import Client
+from aioredis import create_pool
 
-from chanel.config import (VAULT_ADDRESS, VAULT_TOKEN, RUN_ENV, GITHUB_TOKEN)
+from chanel.config import (VAULT_ADDRESS, VAULT_TOKEN, RUN_ENV, GITHUB_TOKEN, SERVICE_NAME)
 
 
 class VaultClient:
+    _redis_credential = None
+
     @classmethod
     def initialize(cls):
         cls.client = Client(url=VAULT_ADDRESS)
@@ -12,4 +16,25 @@ class VaultClient:
             cls.client.token = VAULT_TOKEN
         elif GITHUB_TOKEN:
             cls.client.auth.github.login(token=GITHUB_TOKEN)
+
+    def __getattr__(self, item):
+        try:
+            return self.client.read(f"service-secret/{RUN_ENV}/{SERVICE_NAME}")["data"][item]
+        except KeyError as e:
+            raise Exception(f"{item} is can't be fetched")
+        except Exception as e:
+            raise e
+
+    @property
+    def redis_credential(self):
+        pass
+
+    @staticmethod
+    def create_redis_connection_pool():
+        pool = await create_pool(
+            f"""redis://{getattr(VaultClient, "REDIS_HOST")}""",
+            password=str(getattr(VaultClient, "REDIS_PASSWORD")),
+            encoding="UTF-8"
+        )
+        # TODO Completing the Redis connection pool
 
