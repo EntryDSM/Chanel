06a1992fdd8b4055863f6133fc63ef9f61e75599
Jaehoon Kim <by09115@outlook.kr>
2019-07-16 14:39:40 +0900

    ADD: docker-deploy.sh 추가

---
+++ docker-deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+version='python -c "import chanel; print(chanel.__version__)"'
+
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin regisry.entrydsm.hs.kr
+
+if [[ "$1" == "dev" ]];then
+    echo "Docker build on dev started"
+
+    docker build -t registry.entrydsm.hs.kr/chanel:dev .
+
+    docker push registry.entrydsm.hs.kr/chanel:dev
+elif [[ "$1" == "master" ]];then
+    echo "Docker build on master started"
+
+    docker build -t registry.entrydsm.hs.kr/chanel:${version} .
+
+    docker tag registry.entrydsm.hs.kr/chanel:${version} registry.entrydsm.hs.kr/hermes:latest
+
+    docker push registry.entrydsm.hs.kr/chanel:${version}
+    docker push registry.entrydsm.hs.kr/chanel:latest
+
+fi
+
+exit
