de4596b964fa3188961412eed985cd4b7f3a40db
Jaehoon Kim <by09115@outlook.kr>
2019-08-08 16:53:00 +0900

    ADD: redis, vault 연동

---
+++ chanel/common/client/redis.py
@@ -0,0 +1,44 @@
+from ujson import dumps, loads
+from aioredis import Redis, create_redis_pool
+
+
+class RedisClient:
+    redis: Redis = None
+
+    @classmethod
+    async def initialize(cls, connection_info) -> Redis:
+        if not cls.redis:
+            cls.redis = await create_redis_pool(**connection_info)
+
+        return cls.redis
+
+    @classmethod
+    async def destroy(cls) -> None:
+        cls.redis.close()
+        await cls.redis.wait_closed()
+
+        cls.redis = None
+
+    @classmethod
+    async def set(cls, key: str, value: str, expire: int = None) -> None:
+        await cls.redis.set(key=key, value=dumps(value), expire=expire)
+
+    @classmethod
+    async def set_pair(cls, key: str, value: str, expire: int = None) -> None:
+        await cls.set(key=key, value=dumps(value), expire=expire)
+        await cls.set(key=dumps(value), value=key, expire=expire)
+
+    @classmethod
+    async def get(cls, key: str):
+        temp = await cls.redis.get(key)
+        return loads(temp) if temp else None
+
+    @classmethod
+    async def delete(cls, key: str) -> None:
+        await cls.redis.delete(key)
+
+    @classmethod
+    async def delete_pair(cls, key: str) -> None:
+        temp = await cls.get(key)
+        await cls.delete(temp)
+        await cls.delete(key)
---
+++ chanel/common/client/vault.py
@@ -0,0 +1,70 @@
+from typing import Dict
+
+from hvac import Client
+from hvac.exceptions import InvalidRequest
+from sanic.log import logger
+
+from chanel.common.constant import GITHUB_TOKEN, RUN_ENV, VAULT_ADDRESS, SERVICE_NAME
+
+
+class VaultClient:
+    _redis_credential: Dict = None
+    vault_client: Client = None
+
+    @classmethod
+    def initialize(cls):
+
+        if GITHUB_TOKEN and not cls.vault_client:
+            cls.vault_client = Client(VAULT_ADDRESS)
+            cls.vault_client.auth.github.login(token=GITHUB_TOKEN)
+
+            logger.info("Vault: initialized") \
+                if cls.vault_client.is_authenticated() \
+                else logger.error("Vault: not authenticated")
+
+        else:
+            logger.error("Vault: failed to initialize")
+
+    @classmethod
+    def __getattr__(cls, item: str):
+        try:
+            return cls.vault_client.read(f"/service-secret/{RUN_ENV}/{SERVICE_NAME}")["data"][item]
+
+        except InvalidRequest as e:
+            logger.error("Vault: missing client token")
+            raise e
+
+        except KeyError as e:
+            logger.error(f"Vault: keyword {item} doesn't exist")
+            raise e
+
+        except TypeError as e:
+            logger.error(f"Vault: check the path")
+            raise e
+
+        except Exception as e:
+            raise e
+
+
+class Setting:
+    vault_client: VaultClient = None
+
+    def __init__(self, vault_client: VaultClient):
+        self.vault_client = vault_client
+
+    def __getattr__(self, item):
+        return self.vault_client.__getattr__(item)
+
+    @property
+    def redis_connection_info(self):
+        return {
+            "address": f"redis://:{self.vault_client.REDIS_PASSWORD}@{self.vault_client.REDIS_HOST}:"
+                       f"{self.vault_client.REDIS_PORT}",
+            "minsize": 5,
+            "maxsize": 10
+        }
+
+    DEBUG = False if RUN_ENV == "prod" else True
+
+
+settings = Setting(VaultClient())
