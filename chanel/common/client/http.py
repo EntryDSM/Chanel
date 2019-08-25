92ee887347cf500fbf524aa67ed1d77780b1e250
Jaehoon Kim <by09115@outlook.kr>
2019-08-25 10:51:18 +0900

    FIX: HttpClient, RedisClient initialize 방식 변경

--- chanel/common/client/http.py
+++ chanel/common/client/http.py
@@ -1,19 +1,21 @@
-from aiohttp import ClientSession, TCPConnector, ClientTimeout, ClientResponse
+from aiohttp import ClientSession, TCPConnector, ClientTimeout
 
 
 class HTTPClient:
     _session: ClientSession = None
 
     @classmethod
-    def __init__(cls):
-        conn = TCPConnector(limit=100)
-        timeout = ClientTimeout(total=10)
-
-        cls._session = ClientSession(connector=conn, timeout=timeout)
+    async def init(cls):
+        await cls.get_session()
 
     @classmethod
     async def get_session(cls) -> ClientSession:
-        return cls._session if cls._session else None
+        if not cls._session:
+            conn = TCPConnector(limit=100)
+            timeout = ClientTimeout(total=10)
+            cls._session = ClientSession(connector=conn, timeout=timeout)
+
+        return cls._session
 
     @classmethod
     async def destroy(cls) -> None:
@@ -23,31 +25,28 @@ class HTTPClient:
         cls._session = None
 
     @classmethod
-    async def get(cls, url: str, headers: dict = None, **kwargs) -> ClientResponse:
-        async with cls._session.get(
-            url=url,
-            headers=headers,
-            param=kwargs,
-            raise_for_status=True
+    async def get(cls, url: str, **kwargs) -> dict:
+        session = await cls.get_session()
+
+        async with session.get(
+            url, params=kwargs
         ) as response:
-            return response
+            return dict(data=await response.json(), status=response.status)
 
     @classmethod
-    async def post(cls, url: str, headers: dict = None, **kwargs) -> ClientResponse:
-        async with cls._session.post(
-            url=url,
-            headers=headers,
-            param=kwargs,
-            raise_for_status=True
+    async def post(cls, url: str, **kwargs) -> dict:
+        session = await cls.get_session()
+
+        async with session.post(
+            url, params=kwargs
         ) as response:
-            return response
+            return dict(data=await response.json(), status=response.status)
 
     @classmethod
-    async def patch(cls, url: str, headers: dict = None, **kwargs) -> ClientResponse:
-        async with cls._session.patch(
-            url=url,
-            headers=headers,
-            param=kwargs,
-            raise_for_status=True
+    async def patch(cls, url: str, **kwargs) -> dict:
+        session = await cls.get_session()
+
+        async with session.patch(
+            url, params=kwargs
         ) as response:
-            return response
+            return dict(data=await response.json(), status=response.status)
