e5660c97030c1654e1156d3d8b17be1f02643df5
Jaehoon Kim <by09115@outlook.kr>
2019-08-25 10:57:27 +0900

    ADD: sanic-jwt-extended 설정값 & HTTPClient init() 추가

--- chanel/app.py
+++ chanel/app.py
@@ -1,9 +1,11 @@
 import os
+import datetime
 
-from entry_logger_sanic import set_logger
 from sanic import Sanic
+from entry_logger_sanic import set_logger
 from sanic_jwt_extended import JWTManager
 
+from chanel.common.client.vault import settings
 from chanel.admin.controller import admin_blueprint
 from chanel.applicant.controller import applicant_blueprint
 from chanel.common.constant import SERVICE_NAME
@@ -21,6 +23,10 @@ def create_app():
 
     _app.blueprint(applicant_blueprint)
     _app.blueprint(admin_blueprint)
+
+    _app.config['JWT_SECRET_KEY'] = settings.jwt_secret_key
+    _app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=int(settings.jwt_access_expire))
+    _app.config['RBAC_ENABLE'] = True
 
     JWTManager(_app)
 
--- chanel/common/listener.py
+++ chanel/common/listener.py
@@ -1,16 +1,23 @@
-from sanic import app
 from sanic.log import logger
 
 from chanel.common.client.redis import RedisConnection
 from chanel.common.client.vault import settings
+from common.client.http import HTTPClient
 
 
-async def initialize(app: app, loop):
-    await RedisConnection.initialize(settings.redis_connection_info)
+async def initialize(app, loop):
+    redis_connection_info = (
+        app.redis_connection_info
+        if hasattr(app, "redis_connection_info")
+        else settings.redis_connection_info
+    )
+
+    await RedisConnection.init(redis_connection_info)
+    await HTTPClient.init()
+
     logger.info("successfully initialized redis client")
 
 
-async def finalize(app: app, loop):
+async def finalize(app, loop):
     await RedisConnection.destroy()
-
     logger.info("successfully finalized redis client")
