90f1556ff90c601bb804382912c1863b28d4503f
Jaehoon Kim <by09115@outlook.kr>
2019-09-05 20:09:14 +0900

    FIX: string formatting 수정

--- chanel/common/constant.py
+++ chanel/common/constant.py
@@ -15,5 +15,6 @@ GET_APPLICANT_AUTH = HERMES + "/applicant/{0}/authorization"
 ONE_ADMIN = HERMES + "/admin/{0}"
 GET_ADMIN_AUTH = HERMES + "/admin/{0}/authorization"
 
+# TODO 프론트측 html 양식 불러와서 적용시키기
 VERIFY_EMAIL_TITLE = "[대덕소프트웨어마이스터고등학교] 입학전형시스템 회원가입 인증코드"
-VERIFY_EMAIL_CONTENT = "대덕소프트웨어마이스터고등학교 입학전형시스템에 가입하기 위해 다음 코드를 입력해주세요. %s"
+VERIFY_EMAIL_CONTENT = "대덕소프트웨어마이스터고등학교 입학전형시스템에 가입하기 위해 다음 코드를 입력해주세요. 인증코드는 {} 입니다."
