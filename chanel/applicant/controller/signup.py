e2bf3081e4411dbbe051aa0fd7d1bceb0a0ee23f
Jaehoon Kim <by09115@outlook.kr>
2019-09-05 20:02:05 +0900

    FIX: Repository 잘못 참조해오던 것을 수정 - ApplicantCacheRepository -> TempApplicantCacheRepository

--- chanel/applicant/controller/signup.py
+++ chanel/applicant/controller/signup.py
@@ -3,12 +3,13 @@ from sanic.request import Request
 from sanic.views import HTTPMethodView
 from sanic.response import HTTPResponse, json
 
-from chanel.applicant.domain.applicant import ApplicantCacheRepository
-from chanel.applicant.service.signup import SignUpService
-from chanel.common import json_verify, query_parameter_verify
 from chanel.common.client.http import HTTPClient
 from chanel.common.client.redis import RedisConnection
+from chanel.common import json_verify, query_parameter_verify
 from chanel.common.external_sevice import ExternalServiceRepository
+from chanel.applicant.service.signup import SignUpService
+from chanel.applicant.domain.temp_applicant import TempApplicant
+from chanel.applicant.domain.temp_applicant import TempApplicantCacheRepository
 
 signup_bp: Blueprint = Blueprint("applicant_signup")
 
@@ -31,11 +32,14 @@ class VerifyApplicantEmail(HTTPMethodView):
 
     service = SignUpService(
         ExternalServiceRepository(HTTPClient),
-        ApplicantCacheRepository(RedisConnection)
+        TempApplicantCacheRepository(RedisConnection)
     )
 
     async def post(self, request: Request) -> HTTPResponse:
-        response = await self.service.send_verify_email(request)
+        resend = True if request.args.get("resend") else None
+
+        temp_applicant = TempApplicant(request.json.get("email")).generate_verify_code()
+        response = await self.service.send_verify_email(temp_applicant, resend)
 
         return response
 
