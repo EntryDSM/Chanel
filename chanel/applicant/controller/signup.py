d69e84fc608ff0b79c615679bc5bc45ca5831bf7
Jaehoon Kim <by09115@outlook.kr>
2019-09-11 23:56:07 +0900

    ADD: 인증코드 검증/회원가입 endpoint 작성 - 클래스 이름 변경 - check_email_syntax 적용

--- chanel/applicant/controller/signup.py
+++ chanel/applicant/controller/signup.py
@@ -5,30 +5,18 @@ from sanic.response import HTTPResponse, json
 
 from chanel.common.client.http import HTTPClient
 from chanel.common.client.redis import RedisConnection
-from chanel.common import json_verify, query_parameter_verify
+from chanel.common import json_verify, query_parameter_verify, BadRequest
 from chanel.common.external_sevice import ExternalServiceRepository
 from chanel.applicant.service.signup import SignUpService
 from chanel.applicant.domain.temp_applicant import TempApplicant
 from chanel.applicant.domain.temp_applicant import TempApplicantCacheRepository
+from chanel.common.mail import check_email_syntax
 
 signup_bp: Blueprint = Blueprint("applicant_signup")
 
 
-class CreateApplicantTempAccount(HTTPMethodView):
-
-    async def post(self, request: Request) -> HTTPResponse:
-        return json(dict(msg="say hello"), 200)
-
-
-class VerifyApplicantSignUpCode(HTTPMethodView):
-    decorators = [query_parameter_verify(["code"])]
-
-    async def get(self, request: Request) -> HTTPResponse:
-        return json(dict(msg="say hello"), 200)
-
-
-class VerifyApplicantEmail(HTTPMethodView):
-    decorators = [json_verify({"email": str})]
+class CreateApplicantAccount(HTTPMethodView):
+    decorators = [json_verify(dict(email=str, password=str)), check_email_syntax()]
 
     service = SignUpService(
         ExternalServiceRepository(HTTPClient),
@@ -36,7 +24,42 @@ class VerifyApplicantEmail(HTTPMethodView):
     )
 
     async def post(self, request: Request) -> HTTPResponse:
-        resend = True if request.args.get("resend") else None
+        email = request.json.get("email")
+        password = request.json.get("password")
+
+        response = await self.service.create_account(email, password)
+
+        return response
+
+
+class VerifyApplicantSignUpCode(HTTPMethodView):
+    decorators = [
+        query_parameter_verify(["code"]),
+        check_email_syntax(email_url_index=8)
+    ]
+
+    service = SignUpService(
+        ExternalServiceRepository(HTTPClient),
+        TempApplicantCacheRepository(RedisConnection)
+    )
+
+    async def get(self, request: Request, email, verify_code) -> HTTPResponse:
+        temp_applicant = TempApplicant(email, verify_code)
+        response = await self.service.check_verify_code(temp_applicant)
+
+        return response
+
+
+class VerifyApplicantEmail(HTTPMethodView):
+    decorators = [json_verify({"email": str}), check_email_syntax()]
+
+    service = SignUpService(
+        ExternalServiceRepository(HTTPClient),
+        TempApplicantCacheRepository(RedisConnection)
+    )
+
+    async def post(self, request: Request) -> HTTPResponse:
+        resend = True if request.args.get("resend") else False
 
         temp_applicant = TempApplicant(request.json.get("email")).generate_verify_code()
         response = await self.service.send_verify_email(temp_applicant, resend)
@@ -44,6 +67,6 @@ class VerifyApplicantEmail(HTTPMethodView):
         return response
 
 
-signup_bp.add_route(CreateApplicantTempAccount.as_view(), "/signup")
-signup_bp.add_route(VerifyApplicantSignUpCode.as_view(), "/signup/verify")
+signup_bp.add_route(CreateApplicantAccount.as_view(), "/signup")
+signup_bp.add_route(VerifyApplicantSignUpCode.as_view(), "/signup/verify/<email>/<verify_code>")
 signup_bp.add_route(VerifyApplicantEmail.as_view(), "/signup/verify")
